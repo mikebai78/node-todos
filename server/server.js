ace060cee2d7ac4d1ddb9a12495996013ae256c2
MB <mb@gmail.com>
2018-06-10 09:22:06 -0600

    ready for heroku deploy

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
-
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -47,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('started on port 3000');
+app.listen(port, () => {
+  console.log(`started on port ${port}.`);
 })
 
 module.exports = {app};
