653bacab3c6ed580b5d9797b8775b345bd5546d8
MB <mb@gmail.com>
2018-06-11 13:51:55 -0600

    add config file

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+const env = process.env.NODE_ENV || 'development';
+
+if(env === 'development'){
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApi'
+} else if (env === 'test'){
+  process.env.PORT =3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApiTest'
+}
+
+module.exports.config = config
