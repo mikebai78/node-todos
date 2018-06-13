60d43fb270fc755a6c22b56e476415d84beadbfb
MB <mb@gmail.com>
2018-06-13 12:23:05 -0600

    add post /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -107,6 +108,18 @@ app.post('/users', (req, res) => {
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 })
+
+app.post('/users/login',(req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send({user});
+      })
+  }).catch((err) => {
+  res.status(400).send();
+  })
+});
 
 
 app.listen(port, () => {
