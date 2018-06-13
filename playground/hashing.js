0f50d22fa17250b844d5f15b39fce4664e18edd7
MB <mb@gmail.com>
2018-06-12 18:08:25 -0600

    hash password

--- playground/hashing.js
+++ playground/hashing.js
@@ -2,6 +2,22 @@ const {SHA256} = require('crypto-js');
 
 const jwt = require('jsonwebtoken');
 
+const bcrypt = require('bcryptjs');
+
+let password = 'abc123';
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  })
+})
+
+let hashedPassword = '$2a$10$aP6yWaV9gJc76tK8oTqQhOkm4U9sTAC5dCab0b0XO7F480YwMuAOO';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
+
 // let message = 'I am user number 3';
 //
 // let hash = SHA256(message).toString();
@@ -9,17 +25,17 @@ const jwt = require('jsonwebtoken');
 // console.log(`Message: ${message}`);
 // console.log(`Hash: ${hash}`);
 
-
-jwt.sign
-jwt.verify
-
-let data = {
-  id:10
-};
-
-let token = jwt.sign(data, '123abc');
-
-console.log(token);
-
-let decoded = jwt.verify(token, '123abc');
-console.log('decoded:',decoded);
+//
+// jwt.sign
+// jwt.verify
+//
+// let data = {
+//   id:10
+// };
+//
+// let token = jwt.sign(data, '123abc');
+//
+// console.log(token);
+//
+// let decoded = jwt.verify(token, '123abc');
+// console.log('decoded:',decoded);
--- server/server.js
+++ server/server.js
@@ -96,7 +96,7 @@ app.post('/users', (req, res) => {
   let body = _.pick(req.body, ['email','password','name','age'])
   let user = new User(body);
   user.save().then(() => {
-    return user.generateAuthToken()
+    return user.generateAuthToken();
   }).then((token) => {
     res.header('x-auth', token).send({user});
   }).catch((err) => {
